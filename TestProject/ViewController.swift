49515e6a86d31d525368a2c64eedab2cc620d13d
Ryan Chew <39399@cgs.act.edu.au>
2015-07-24 12:51:32 +1000

    Initial commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by CRyan7 on 24/7/15.
+//  Copyright (c) 2015 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    println("Hello World!")
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
