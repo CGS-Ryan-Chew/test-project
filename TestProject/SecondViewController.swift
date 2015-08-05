bae218128d379b20fa352fad326d7f0e90451602
Ryan Chew <39399@cgs.act.edu.au>
2015-08-05 11:22:01 +1000

    Added view controllers and fancy functions

---
+++ TestProject/SecondViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SecondViewController.swift
+//  TestProject
+//
+//  Created by CRyan7 on 5/8/15.
+//  Copyright (c) 2015 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        //Fancy Functions for days
+        for i in 1..10 {
+            println("Sup Bruh")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
